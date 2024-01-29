a2a15699c851bcec0a9f95143c0549d8c8752989
vinhjaxt <ngoc.lo.dang@gmail.com>
2024-01-29 09:40:54 +0000

    Update main.go

--- main.go
+++ main.go
@@ -4,45 +4,32 @@ import (
 	"flag"
 	"log"
 	"net"
+	"io"
+	"time"
 
 	"h12.io/socks"
 )
 
 var lAddr = flag.String(`l`, `127.0.0.1:3388`, `Listen address`)
-var socksURI = flag.String(`x`, `socks5://127.0.0.1:1080?timeout=5m`, `Socks URI`)
+var socksURI = flag.String(`x`, `socks5://127.0.0.1:1080?timeout=15m`, `Socks URI`)
 var proxyAddr = flag.String(`r`, `10.10.10.10:3389`, `Remote address`)
 var dialFunc func(string, string) (net.Conn, error)
 
-func copy2(dst net.Conn, src net.Conn) {
-	defer dst.Close()
-	defer src.Close()
-	buf := make([]byte, 4096)
-	for {
-		n, err := src.Read(buf)
-		if err != nil {
-			// log.Println(`Read err:`, err)
-			return
-		}
-		// log.Println(src.RemoteAddr().String(), `=>`, dst.RemoteAddr().String(), `:`, len(buf[:n]))
-		_, err = dst.Write(buf[:n])
-		if err != nil {
-			// log.Println(`Write errL`, err)
-			return
-		}
-	}
-}
-
 func handleConn(lconn net.Conn) {
-	defer lconn.Close()
 	rconn, err := dialFunc(`tcp`, *proxyAddr)
 	if err != nil {
+		lconn.Close()
 		log.Println(err)
 		return
 	}
 	log.Println(`Connected to`, *proxyAddr)
-	defer rconn.Close()
-	go copy2(lconn, rconn)
-	copy2(rconn, lconn)
+	defer func () {
+		time.Sleep(time.Second)
+		rconn.Close()
+		lconn.Close()
+	}()
+	go io.Copy(lconn, rconn)
+	io.Copy(rconn, lconn)
 	log.Println(`Closed:`, lconn.RemoteAddr().String())
 }
 
